8957e2f83f6369a97e389425618cd485ed238749
thalys_yangwenhao <yangwenhao@thalys.net.cn>
2021-04-15 11:24:22 +0800

    log-plugin2

--- plugin/src/main/groovy/com/ywh/plugin/MyPlugin.java
+++ plugin/src/main/groovy/com/ywh/plugin/MyPlugin.java
@@ -1,17 +1,12 @@
 package com.ywh.plugin;
 
-import android.util.Log;
-
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.Task;
 
 public class MyPlugin  implements Plugin<Project> {
     // 当插件apply时调用
     @Override
     public void apply(Project project) {
         System.out.println("--------apply MyPlugin---------");
-
     }
 }
